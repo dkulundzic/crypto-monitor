60a0874e769abf6392dd1a82db6d0400e772e314
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2025-01-24 11:47:31 +0100

    feat: Expanded the Model by adding the ExchangeRateMO

---
+++ WB-Test-Template/App/Common/Data/Core Data/Models/ExchangeRateMO.swift
@@ -0,0 +1,25 @@
+import Foundation
+import CoreData
+
+@objc(ExchangeRateMO)
+final class ExchangeRateMO: NSManagedObject {
+    @NSManaged var time: Date
+    @NSManaged var rate: Double
+    @NSManaged var assetIdQuote: String?
+
+    @nonobjc static func fetchRequest() -> NSFetchRequest<ExchangeRateMO> {
+        NSFetchRequest(entityName: "ExchangeRate")
+    }
+}
+
+extension ExchangeRateMO: Identifiable { }
+
+extension ExchangeRateMO: DomainObjectTransformable {
+    func toDomain() -> ExchangeRate {
+        .init(
+            time: time,
+            rate: rate,
+            assetIdQuote: assetIdQuote
+        )
+    }
+}
