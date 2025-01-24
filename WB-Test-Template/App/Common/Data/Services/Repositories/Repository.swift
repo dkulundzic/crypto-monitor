8691f374b9b308d49135a0cc80d80fb21a567e3c
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2025-01-24 11:22:46 +0100

    chore: Improved @RepositoryActor usage

--- WB-Test-Template/App/Common/Data/Services/Repositories/Repository.swift
+++ WB-Test-Template/App/Common/Data/Services/Repositories/Repository.swift
@@ -5,15 +5,13 @@ import CoreData
     static var shared = RepositoryActor()
 }
 
-@RepositoryActor
 protocol Repository {
     associatedtype Model
-    nonisolated init()
-    func fetch(id: String) async throws -> Model?
-    func fetchAll() async throws -> [Model]
-    func save(_ model: Model) async throws
-    func save(_ models: [Model]) async throws
-    func delete(_ id: String) async throws
+    @RepositoryActor func fetch(id: String) async throws -> Model?
+    @RepositoryActor func fetchAll() async throws -> [Model]
+    @RepositoryActor func save(_ model: Model) async throws
+    @RepositoryActor func save(_ models: [Model]) async throws
+    @RepositoryActor func delete(_ id: String) async throws
 }
 
 protocol DomainObjectTransformable {
